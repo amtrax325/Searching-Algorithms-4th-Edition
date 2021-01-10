30116cc16338b5bbe5d091d5ce0f90909852dfff
Miko <mikics2k17@gmail.com>
2021-01-10 13:28:41 +0100

    Fixed and upgraded FrequencyCounter's method of getting words frome file. Also uploaded better version of tale.txt file which includes data for project.
    FrequencyCounter "getting" loop does not include characters like  . ? ;  * ! - : " , [space] in saved words.

--- src/com/Clients/FrequencyCounter.java
+++ src/com/Clients/FrequencyCounter.java
@@ -8,26 +8,43 @@ import java.util.Scanner;
 
 public class FrequencyCounter {
     public static void main(String[] args) throws FileNotFoundException {
-//TODO: better implementation of function which gets words frome .txt file
+
         BinarySearchST<String,Integer> binarySearch = new BinarySearchST<>(1);
         File file = new File("tale.txt");
         Scanner in = new Scanner(file);
 
-        while(in.hasNext())
+        int minlen = 8;
+
+        while (in.hasNextLine())
         {
-            String word = in.next();
-            if(word.length() < 0)  continue;
-            if(!binarySearch.contains(word)) binarySearch.put(word,1);
-            else binarySearch.put(word,binarySearch.get(word)+1);
+            String word = in.nextLine();
+
+            word = word.replaceAll(",|\\.|\\?|;|\\*|!|-|:|\"","");
+            String [] words = word.split("\\s");
+
+            for (String w : words)
+            {
+                if(w.isEmpty())
+                     continue;
+                if(w.length() < minlen) continue;
+                if(!binarySearch.contains(w)) binarySearch.put(w,1);
+                 else binarySearch.put(w,binarySearch.get(w)+1);
+            }
         }
         String max = "";
+
         binarySearch.put(max,0);
+
         for (String word : binarySearch.keys())
             if (binarySearch.get(word) > binarySearch.get(max))
                 max = word;
 
         System.out.println(binarySearch.size());
         System.out.println(max + " " + binarySearch.get(max));
+
+
+
+
 
     }
 
