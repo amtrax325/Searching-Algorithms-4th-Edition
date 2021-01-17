f08994798dd67d9075e5912d77f2d4e28f45863d
Miko <mikics2k17@gmail.com>
2021-01-17 14:22:17 +0100

    Revert "Added new deleteST method, which is recurrent edition of delete method. Old delete method has been Deprecated"
    This reverts commit a0f18565

--- src/com/search/SequentialSearchST.java
+++ src/com/search/SequentialSearchST.java
@@ -1,7 +1,12 @@
 package com.search;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class SequentialSearchST<Key, Value>
 {
+
+    private int size = 0;
     private Node first;
     private class Node
     {
@@ -22,11 +27,52 @@ public class SequentialSearchST<Key, Value>
         return x.val;
         return null;
     }
+
+    public void delete (Key key)
+    {
+    if(key.equals(first.key))
+    {
+        first  = first.next;
+        size--;
+        return;
+    }
+    Node previous = first;
+    for(Node x = first.next; x != null; x = x.next)
+    {
+        if (key.equals(x.key))
+        {
+            previous.next = x.next;
+            size--;
+            return;
+        }
+        previous = x;
+    }
+
+
+    }
+    public int size()
+    {
+        return size;
+    }
+    public Iterable<Key> keys()
+    {
+        List<Key> q = new LinkedList<>();
+        for (Node x = first; x!= null; x = x.next)
+        {
+            q.add(x.key);
+        }
+        return q;
+    }
+
+
     public void put(Key key, Value val)
     {
         for (Node x = first; x != null; x = x.next)
             if (key.equals ( x.key))
         { x.val = val; return; }
+
+
         first = new Node(key, val, first);
+            size++;
     }
 }
